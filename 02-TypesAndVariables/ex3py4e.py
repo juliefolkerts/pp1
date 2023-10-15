f18d08deb065dd505ec35fa47b0001bf71588a4a
Julie Folkerts <JulieVMF@gmail.com>
2023-10-15 15:41:36 +0200

    exercise py4e

---
+++ 02-TypesAndVariables/ex3py4e.py
@@ -0,0 +1,4 @@
+hours = int(input('How many hours do you work a month?\n'))
+rate = float(input('What is your rate?\n'))
+pay = hours * rate
+print('Your pay is', pay)
---
+++ 02-TypesAndVariables/ex5py4e.py
@@ -0,0 +1,3 @@
+Celsius = int(input('How many degrees Celsius?\n'))
+Fahrenheit = ((Celsius * (9/5)) + 32)
+print(Fahrenheit)
