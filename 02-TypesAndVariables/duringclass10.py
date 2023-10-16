2d56d5eeecfb2973c5c5ce15a14da3de19697715
Julie Folkerts <JulieVMF@gmail.com>
2023-10-16 09:46:49 +0200

    push

--- 02-TypesAndVariables/duringclass10.py
+++ 02-TypesAndVariables/duringclass10.py
@@ -1,5 +1,4 @@
 name = 'Julie'
 age = 19
 height = 174
-s = F'My name is {name}, I am {age} years old, and my height is {height} cm. In 6 years I will be {age + 6}'
-print(s)
+print(F'My name is {name}, I am {age} years old, and my height is {height} cm. In 6 years I will be {age + 6}')
--- 02-TypesAndVariables/duringclass11.py
+++ 02-TypesAndVariables/duringclass11.py
@@ -1,5 +1,5 @@
 a = int(input('Enter fathers income: '))
 b = int(input('Enter mothers income: '))
 c = int(input('Enter number of people in family: '))
-total income = a + b
-print(total income)
+total_income = a + b
+print(total_income)
