093397cb6f9ef43f41c2948c4c7b12e087917470
Julie Folkerts <JulieVMF@gmail.com>
2023-10-17 18:14:58 +0200

    push

--- 02-TypesAndVariables/afterclass18.py
+++ 02-TypesAndVariables/afterclass18.py
@@ -1,7 +1,7 @@
 x = 7
 y = 34
-print('Value of x is: 7') #why doesn't it work to put x in this?
-print('Value of y is: 34') #same here?
+print(f'Value of x is: {x}') 
+print(f'Value of y is: {y}') 
 
 temp = x
 x = y
