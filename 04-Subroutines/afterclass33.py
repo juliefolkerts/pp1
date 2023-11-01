fe5e3c2f6cea5d062fca9176600f7e2d4a842938
Julie Folkerts <JulieVMF@gmail.com>
2023-11-01 20:08:40 +0100

    push

---
+++ 04-Subroutines/afterclass33.py
@@ -0,0 +1,5 @@
+def f(n):
+    return "*/" * (n - 1) + "*"
+
+n = int(input('Enter amount:'))
+print(f(n))
---
+++ 04-Subroutines/afterclass34.py
@@ -0,0 +1,9 @@
+def f(n):
+    number = ""
+    for i in range(1,n+1):
+        number += str(i)
+    return number
+
+result = f(11)
+print(result)
+        
