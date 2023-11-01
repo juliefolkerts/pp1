c400aa914a008ca2f958d46352be9c43641eaa57
Julie Folkerts <JulieVMF@gmail.com>
2023-11-01 19:03:16 +0100

    push

--- 04-Subroutines/afterclass30.py
+++ 04-Subroutines/afterclass30.py
@@ -1,6 +1,15 @@
 def f(number, even):
-    if even == True:
-        for 
+    digit_sum = 0
+    for digit in str(number):
+        digit = int(digit)
+        if (even == True and digit % 2 == 0) or (even == False and digit % 2 != 0):
+            digit_sum += digit
+    return digit_sum
+
+result = f(3124, True)
+print(result)
+
+        
 
 
     #for digit in number:
---
+++ 04-Subroutines/afterclass31.py
@@ -0,0 +1,11 @@
+def f(x, y):
+    count = 0
+    for i in range (x, y+1):
+        if i % 2 == 0 and i < 0:
+            count += 1
+    return count
+
+result = (f(-7, 8))
+
+print(result)
+        
