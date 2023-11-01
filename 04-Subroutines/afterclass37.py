cb79ea243ddfb989e6550e4f9dafe5f7477b69bd
Julie Folkerts <JulieVMF@gmail.com>
2023-11-01 20:54:41 +0100

    push

--- 04-Subroutines/afterclass37.py
+++ 04-Subroutines/afterclass37.py
@@ -1,11 +1,10 @@
 def f(n):
     if n <= 0:
-        return "Invalid input. n should be a positive integer."
-    elif n == 1:
         return 0
-    elif n == 2:
+    elif n == 1:
         return 1
     else:
-        return f(n-1) + f(n-2)
+        return f(n - 1) + f(n - 2)
     
-print(f())
+result = f(9)
+print(result)
