7feae7223ba69c9644b1d4ac488fa805d842cf7c
Julie Folkerts <JulieVMF@gmail.com>
2023-11-03 09:19:02 +0100

    push

---
+++ 04-Subroutines/afterclass44.py
@@ -0,0 +1,18 @@
+def f(password):
+    password = password.lower()
+    lengt_password = int(len(password))
+    for i in password:
+        frequency = password.count(i)
+        if frequency > 1:
+            lengt_password = (lengt_password - frequency + 1)
+        else:
+            lengt_password = lengt_password
+    if lengt_password >= 6:
+        return True
+    else:
+        return False
+
+result = f('1234AB')
+print(result)
+    
+        
