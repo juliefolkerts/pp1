7f8778ef5ce9e8d4b108d2c56c7af42bdcf90822
Julie Folkerts <JulieVMF@gmail.com>
2023-11-06 23:32:49 +0100

    push

--- 03-ControlStructures/afterclass47.py
+++ 03-ControlStructures/afterclass47.py
@@ -1,7 +1,10 @@
 def lottery():
-    for i in range(1, 50, 7):
-        for j in range(i, i + 7):
-            print(j, end=' ')
+    for i in range(1, 8):
+        for j in range(i, 50, 7):
+            if j < 10:
+                print('',j, end= ' ')
+            else:
+                print(j, end=' ')
         print()
 
-lottery()
+lottery() 
