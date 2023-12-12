275595599a60ce4bcc0089db30956ed880c6a802
Julie Folkerts <JulieVMF@gmail.com>
2023-12-12 22:12:28 +0100

    push

--- 06-Arrays/53.py
+++ 06-Arrays/53.py
@@ -1,3 +1,10 @@
-def id_matrix(n):
-    
-    
+def f(arr):
+    newarr = []
+    for j in range(0,len(arr[0])):
+        row = []
+        for i in range(0,len(arr)):
+            row.append(arr[i][j])
+        newarr.append(row)
+    return newarr
+arr = [[1,2],[3,4],[5,6]]
+print(f(arr))
---
+++ 06-Arrays/p7.2.py
@@ -0,0 +1,8 @@
+def f(e):
+    expression = list(e)
+    result = []
+    for i in range(0, len(expression), 2):
+        
+
+
+print(f(e))
