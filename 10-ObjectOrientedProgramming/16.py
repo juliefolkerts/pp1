b0959c3312c659e36a9880f374ffb6dd0f7eef87
Julie Folkerts <JulieVMF@gmail.com>
2024-01-03 22:09:29 +0100

    push

--- 10-ObjectOrientedProgramming/16.py
+++ 10-ObjectOrientedProgramming/16.py
@@ -10,6 +10,9 @@ class Phone():
     def display_whole_nr(self):
         return self.code + self.nr
     
+    def __str__(self):
+        return f"Phone: {self.display_whole_nr()} - Battery: {self.display_battery()}"
+    
 phone1 = Phone('21265815','+316','80')
 phone2 = Phone('21265815','+316','40')
 print(phone1.display_battery())
