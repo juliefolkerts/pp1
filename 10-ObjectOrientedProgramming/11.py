7e675ce92e68494b7635f7d8f439302ed2c5260e
Julie Folkerts <JulieVMF@gmail.com>
2024-01-03 21:07:29 +0100

    push

--- 10-ObjectOrientedProgramming/11.py
+++ 10-ObjectOrientedProgramming/11.py
@@ -1,7 +1,8 @@
 class TV():
-    def __init__(self,is_on=False,channel_no=1):
+    def __init__(self,is_on=False,channel_no=1,channels=[]):
         self.is_on = is_on 
         self.channel_no = str(channel_no)
+        self.channels = channels
 
     def turn_on(self):
         self.is_on = True
@@ -11,9 +12,18 @@ class TV():
 
     def set_channel(self, new_channel_no):
         self.channel_no = str(new_channel_no)
+
+    def set_channels(self, channels_list):
+        self.channels = channels_list
+
+    def show_channels(self):
+        return self.channels
+
     
     def show_status(self):
-        if self.is_on == True:
+        if self.is_on == True and int(self.channel_no) <= int(len(self.channels)) :
+            return 'On, channel ' + self.channel_no + f' ({self.channels[int(self.channel_no)-1]})'
+        elif self.is_on == True and int(self.channel_no) > int(len(self.channels)) :
             return 'On, channel ' + self.channel_no
         else:
             return 'Off'
@@ -25,11 +35,16 @@ print(tv.show_status())
 tv.turn_on()
 #d
 print(tv.show_status())
-#e
-tv.set_channel(5)
+#e 
+print(tv.show_channels())
 #f
-print(tv.show_status())
+tv.set_channels(['TVP1', 'TVP2', 'Polsat', 'TVN', 'Filmbox', 'Discovery'])
 #g
-tv.turn_off()
+print(tv.show_channels())
+tv.set_channel(1) #if it is 0 it shows discovery how to fix this?
 #h
+print(tv.show_status())
+#i
+tv.turn_off()
+#j
 print(tv.show_status())
