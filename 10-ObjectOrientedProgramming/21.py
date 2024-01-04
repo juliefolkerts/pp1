1962ea74eca63b72ccc42d3538b2b6a7423a2e3a
Julie Folkerts <JulieVMF@gmail.com>
2024-01-04 12:51:06 +0100

    push

--- 10-ObjectOrientedProgramming/21.py
+++ 10-ObjectOrientedProgramming/21.py
@@ -23,13 +23,25 @@ class Stat():
         return (sum(self.numbers) / len(self.numbers))
     
     def median(self):
-        pass
+        self.numbers = sorted(self.numbers)
+        if len(self.numbers) % 2 != 0:
+            i = int(len(self.numbers)/2)
+            self.mediann = self.numbers[i]
+        else:
+            i = (len(self.numbers)/2)
+            self.mediann = (self.numbers[i-1]+self.numbers[i])/2
+
 
     def displaystats(self):
-        return f'max: {self.biggestt}\n min: {self.smallestt}\n mean: {self.meann}'
+        return f'max: {self.biggestt}\nmin: {self.smallestt}\nmean: {self.meann}\nmedian: {self.mediann}'
     
 stat1 = Stat()
 stat1.add(12)
 stat1.add(37)
 stat1.add(6)
 print(stat1.display())
+stat1.biggest()
+stat1.smallest()
+stat1.mean()
+stat1.median()
+print(stat1.displaystats())
