a3fdd592cf627bd4116e8ec7d98d8aae4e3ae8e5
Julie Folkerts <JulieVMF@gmail.com>
2024-01-09 17:17:36 +0100

    push

---
+++ 11-FunctionalProgramming/1818.py
@@ -0,0 +1,7 @@
+limit = lambda x: x > 50
+speeds = [48,47,54,50,42,68,39,46]
+
+result = list(filter(limit,speeds))
+print(f'recorded values:48,47,54,50,42,68,39,46')
+print(f'Speed to high:{','.join(map(str,result))}')
+    
